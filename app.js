71b4dba7694951067834067149602552e2ca3fa2
Twinkle Chhabra <twinkle.chhabra123@gmail.com>
2021-09-10 23:44:53 +0530

    Second Commit

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ app.post("/failure", function(req, res){
   res.redirect("/");
 });
 
-app.listen(process.env.PORT, function(){
+app.listen(process.env.PORT || 3000, function(){
   console.log("Port running on 3000");
 });
 
